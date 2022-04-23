2dfd5ca1b7a53dec773dbfad897fc3db2d4f714d
nazmul162001 <nazmulprime162001@gmail.com>
2022-04-24 00:14:16 +0600

    added creat db , update & delete user in server

--- index.js
+++ index.js
@@ -1,53 +1,93 @@
-const express = require('express')
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const app = express()
-const port = 4000
-
+const express = require('express');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const app = express();
+const port = 4000;
+const cors = require('cors');
 
 //nazmul
 //rWugJwnIFSSzlxEy
 
+app.use(express.json());
+app.use(cors());
 
-
-const uri = "mongodb+srv://nazmul:rWugJwnIFSSzlxEy@cluster0.94ypj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+const uri =
+  'mongodb+srv://nazmul:rWugJwnIFSSzlxEy@cluster0.94ypj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
 
 async function run() {
-  try{
+  try {
     await client.connect();
-    const collection = client.db("notesTaker").collection("notes");
+    const notesCollection = client.db('notesTaker').collection('notes');
+
+
+  
+    // get api to read all notes / read
+    // http://localhost:4000/notes
+    app.get('/notes', async (req, res) => {
+      const q = req.query;
+      const cursor = notesCollection.find({});
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    // create notesTaker
+    //http://localhost:4000/note
+    app.post('/note', async (req, res) => {
+      const data = req.body;
+      // console.log(data);
+      const result = await notesCollection.insertOne(data);
+      res.send(result);
+    });
+
+    
+
+    // update notesTaker
+    // http://localhost:4000/note/6264389e6101999b585d38a2
+    app.put('/note/:id', async (req, res) => {
+      const id = req.params.id;
+      const data = req.body;
+      console.log(data);
+      const filter = { _id: ObjectId(id) };
+      const options = { upsert: true };
+      const updateDoc = {
+        $set: {
+          userName: data.userName,
+          textData: data.textData,
+        },
+      };
+      const result = await notesCollection.updateOne(filter, updateDoc, options);
+      res.send(result)
+    });
+
+    // Delete notesTaker//
+    // http://localhost:4000/note/6264389e6101999b585d38a2
+    app.delete('/note/:id', async(req, res) => {
+      const id = req.params.id
+      const filter = { _id: ObjectId(id) }
+      const result = await notesCollection.deleteOne(filter)
+      res.send(result)
+    })
+
+
 
 
     console.log('connected to db');
-  }
-  finally{
-
+  } finally {
+    ////////////----------//////////
   }
 }
-
 
 run().catch(console.dir);
 
 
-
-
-
-
-// client.connect(err => {
-//   const collection = client.db("test").collection("devices");
-//   // perform actions on the collection object
-//   // client.close();
-//   console.log('connectede');
-// });
-
-
-
-
-
 app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+  res.send('Hello World!');
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+  console.log(`Example app listening at http://localhost:${port}`);
+});
